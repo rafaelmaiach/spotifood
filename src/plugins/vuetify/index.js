0c5fd2bab35d6b1b53c556505a093e82de1f6595
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-08-11 23:43:04 -0300

    chore(vuetify): uses dark theme

--- src/plugins/vuetify/index.js
+++ src/plugins/vuetify/index.js
@@ -5,6 +5,9 @@ import i18n from '../i18n';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		dark: true,
+	},
 	lang: {
 		t: (key, ...params) => i18n.t(key, params), // eslint-disable-line @intlify/vue-i18n/no-dynamic-keys
 	},
